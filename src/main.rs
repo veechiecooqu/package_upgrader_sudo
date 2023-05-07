a772041d037e2b090ba0300c00ee8e0009ca54ce
Nitesh Kumar Debnath <30214255+veechiecooqu@users.noreply.github.com>
2023-05-07 13:15:08 +0530

    Fixes for not actually being able to run the rate mirrors step

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,27 @@
-use chrono::{Duration, Utc};
-use std::fs;
-use std::path::Path;
-use std::process::{Command, Stdio};
+mod updater;
 
 fn main() {
     // Step 1: Print message
     println!("Updating packages, please wait...");
 
     let mirrorlist_path = "/etc/pacman.d/mirrorlist";
-    let can_update = if !Path::new(mirrorlist_path).exists() {
+    let can_update = if !std::path::Path::new(mirrorlist_path).exists() {
         true
     } else {
-        let metadata = fs::metadata(mirrorlist_path).unwrap();
+        let metadata = std::fs::metadata(mirrorlist_path).unwrap();
         let last_modified = metadata.modified().unwrap();
 
-        let duration_since_modified = Utc::now().signed_duration_since::<Utc>(last_modified.into());
+        let duration_since_modified =
+            chrono::Utc::now().signed_duration_since::<chrono::Utc>(last_modified.into());
 
-        duration_since_modified > Duration::days(1)
+        duration_since_modified > chrono::Duration::days(1)
     };
 
     // Run Step 2 only if the condition is met
     if can_update {
-        update_with_fastest_mirrors();
+        updater::update_with_fastest_mirrors();
     }
 
     // Step 3: Always run the package upgrade
-    upgrade_packages();
-}
-
-fn update_with_fastest_mirrors() {
-    // Step 2: Fetch and use the fastest mirrors
-    println!("\x1b[0;33mFetching fastest mirrors, please wait...\x1b[0m");
-    let status = Command::new("doas")
-        .args([
-            "cp",
-            "-b",
-            "--no-preserve=mode,ownership",
-            &(format!(
-                "rate-mirrors arch --fetch-mirrors-timeout {} | psub",
-                300000
-            )),
-            "/etc/pacman.d/mirrorlist",
-        ])
-        .status()
-        .expect("Failed to execute command");
-
-    if status.success() {
-        println!("\x1b[0;32mUsing fastest mirrors.\x1b[0m");
-    } else {
-        println!("\x1b[0;31mFetch failed. Using previously fetched mirrors.\x1b[0m");
-    }
-}
-
-fn upgrade_packages() {
-    // Step 3: Upgrade packages using paru and fisher
-    Command::new("paru")
-        .stdout(Stdio::inherit())
-        .status()
-        .expect("Failed to execute paru");
-
-    Command::new("fish")
-        .args(["-c", "fisher update"])
-        .stdout(Stdio::inherit())
-        .status()
-        .expect("Failed to execute fisher update");
+    updater::upgrade_packages();
 }
---
+++ src/updater.rs
@@ -0,0 +1,85 @@
+
+
+use std::io::{Error, ErrorKind, Write};
+
+use std::path::Path;
+
+use std::process::{Command, Stdio};
+use tempfile::NamedTempFile;
+
+
+fn copy_with_privilege(src: &Path, dest: &Path) -> std::io::Result<()> {
+    let status = Command::new("doas")
+        .args(["cp", src.to_str().unwrap(), dest.to_str().unwrap()])
+        .status()
+        .map_err(|_e| Error::new(ErrorKind::Other, "Failed to execute doas command"))?;
+
+    if status.success() {
+        // Set the file permissions to -rw-r--r--
+        let chmod_status = Command::new("doas")
+            .args(["chmod", "644", dest.to_str().unwrap()])
+            .status()
+            .map_err(|e| {
+                Error::new(
+                    ErrorKind::Other,
+                    format!("Failed to execute doas chmod command: {}", e),
+                )
+            })?;
+
+        if chmod_status.success() {
+            Ok(())
+        } else {
+            Err(Error::new(
+                ErrorKind::PermissionDenied,
+                "Failed to set file permissions",
+            ))
+        }
+    } else {
+        Err(Error::new(
+            ErrorKind::PermissionDenied,
+            "Failed to copy temporary file to destination",
+        ))
+    }
+}
+pub fn update_with_fastest_mirrors() {
+    // Step 2: Fetch and use the fastest mirrors
+    println!("\x1b[0;33mFetching fastest mirrors, please wait...\x1b[0m");
+    let output = Command::new("rate-mirrors")
+        .args(["arch", "--fetch-mirrors-timeout", "300000"])
+        .output()
+        .expect("Failed to execute rate-mirrors command");
+
+    let mut temp_file = NamedTempFile::new().expect("Failed to create temporary file");
+    temp_file
+        .write_all(&output.stdout)
+        .expect("Failed to write to temporary file");
+
+    let temp_path = temp_file.path();
+
+    let status = Command::new("doas")
+        .args(["chown", "root:root", temp_path.to_str().unwrap()])
+        .status()
+        .expect("Failed to change ownership of temporary file");
+
+    if status.success() {
+        let dest_file = "/etc/pacman.d/mirrorlist";
+        copy_with_privilege(temp_path, Path::new(dest_file))
+            .expect("Failed to copy temporary file to destination");
+    } else {
+        eprintln!("Failed to change ownership of temporary file. Cannot update mirrorlist.");
+    }
+}
+
+pub fn upgrade_packages() {
+    // Step 3: Upgrade packages using paru and fisher
+    Command::new("paru")
+        .stdout(Stdio::inherit())
+        .status()
+        .expect("Failed to execute paru");
+
+    Command::new("fish")
+        .args(["-c", "fisher update"])
+        .stdout(Stdio::inherit())
+        .status()
+        .expect("Failed to execute fisher update");
+}
