ca3af6ab83ee215a8879f9ac7ffdcc8ba4df821f
Nitesh Kumar Debnath <30214255+veechiecooqu@users.noreply.github.com>
2023-12-03 12:21:15 +0530

    Adds idle inhibits on logind systems, should ignore non-logind systems silently

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use logind_dbus::LoginManager;
 mod updater;
 
 fn main() {
     // Step 1: Print message
     println!("Updating packages, please wait...");
+    let mut suspend_lock = Err("Did not work");
+    if let Ok(login_manager) = LoginManager::new() {
+        suspend_lock = Ok(login_manager
+            .connect()
+            .inhibit_suspend("Package Upgrader", "Prevents sleep during upgrade"));
+    }
+    if suspend_lock.is_ok() {
+        println!("Idle inhibit ready");
+    }
 
     let mirrorlist_path = "/etc/pacman.d/mirrorlist";
     let can_update = if !std::path::Path::new(mirrorlist_path).exists() {
